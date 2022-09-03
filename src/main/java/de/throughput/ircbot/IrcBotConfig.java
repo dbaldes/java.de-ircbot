d01a752632a819684f5a9f1bcb47b4df2aae20b9
Daniel Baldes <daniel@baldes.name>
2022-09-03 11:42:53 +0700

    configuration to config file

--- src/main/java/de/throughput/ircbot/IrcBotConfig.java
+++ src/main/java/de/throughput/ircbot/IrcBotConfig.java
@@ -1,8 +1,8 @@
 package de.throughput.ircbot;
 
+import java.util.List;
 import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -15,14 +15,69 @@ import lombok.Getter;
 @Getter
 public class IrcBotConfig {
 
-  @Autowired
-  public IrcBotConfig(@Value("${ircbot.talkchannels}") String talkChannels) {
-    this.talkChannels = Set.of(talkChannels.split(","));
-  }
+  private static final int IRC_DEFAULT_PORT = 6667;
+  
+  /**
+   * Server address.
+   */
+  @Value("${ircbot.irc.server}")
+  private String server;
+
+  /**
+   * Server port.
+   */
+  @Value("${ircbot.irc.port}")
+  private int port = IRC_DEFAULT_PORT;
+
+  /**
+   * Use SSL.
+   */
+  @Value("${ircbot.irc.ssl}")
+  private boolean ssl;
+
+  /**
+   * Use TLS.
+   */
+  @Value("${ircbot.irc.tls}")
+  private boolean tls;
+
+  /**
+   * Trust invalid certificates.
+   */
+  @Value("${ircbot.irc.ssl.trustAll}")
+  private boolean trustAll;
+
+  /**
+   * Bot nick.
+   */
+  @Value("${ircbot.irc.nick}")
+  private String nick;
+  
+  /**
+   * Nickserv password.
+   */
+  @Value("#{'${ircbot.irc.nickserv.password}' == '' ? null : '${ircbot.irc.nickserv.password}'}")
+  private String nickservPassword;
+  
+  /**
+   * Channels to join.
+   */
+  @Value("#{'${ircbot.irc.channels}'.split(',')}")
+  private List<String> channels;
   
   /**
    * Channels on which the bot is allowed to talk.
    */
+  @Value("#{'${ircbot.talkchannels}'.split(',')}")
   private Set<String> talkChannels;
-    
+
+  /**
+   * Channels on which the factoid system is active.
+   */
+  @Value("#{'${ircbot.factoid.channels}'.split(',')}")
+  private Set<String> factoidChannels;
+  
+  @Value("#{'${ircbot.admins}'.split(',')}") 
+  private Set<String> admins;
+   
 }
