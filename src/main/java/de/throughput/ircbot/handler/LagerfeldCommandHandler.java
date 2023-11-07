dfe3d3d343fae320113fa4a4165fd714065e18d3
Daniel Baldes <daniel@baldes.name>
2023-11-07 10:26:28 +0700

    lagerfeld quote command

---
+++ src/main/java/de/throughput/ircbot/handler/LagerfeldCommandHandler.java
@@ -0,0 +1,36 @@
+package de.throughput.ircbot.handler;
+
+import java.util.Set;
+import org.springframework.stereotype.Component;
+
+import de.throughput.ircbot.api.Command;
+import de.throughput.ircbot.api.CommandEvent;
+import de.throughput.ircbot.api.CommandHandler;
+
+/**
+ * Lagerfeld command handler.
+ * <p>
+ * !lagerfeld <text> - responds with a Lagerfeld quote.
+ */
+@Component
+public class LagerfeldCommandHandler implements CommandHandler {
+
+    private static final Command CMD_LAGERFELD = new Command("lagerfeld", "lagerfeld <text> - responds with a Lagerfeld quote.");
+
+    @Override
+    public Set<Command> getCommands() {
+        return Set.of(CMD_LAGERFELD);
+    }
+
+    @Override
+    public boolean onCommand(CommandEvent command) {
+        command.getArgLine()
+                .ifPresentOrElse(
+                        text -> command.getEvent()
+                                .getChannel()
+                                .send()
+                                .message("\"" + text + ", hat die Kontrolle über sein Leben verloren.\" -- Karl Lagerfeld."),
+                        () -> command.respond(CMD_LAGERFELD.getUsage()));
+        return true;
+    }
+}
