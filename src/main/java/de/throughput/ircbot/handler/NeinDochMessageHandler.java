d4ed1704b43232e1629a13d7c5edf3bb1fa144d6
Daniel Baldes <daniel@baldes.name>
2024-06-08 07:39:14 +0700

    Simplified the nein/doch handler.

--- src/main/java/de/throughput/ircbot/handler/NeinDochMessageHandler.java
+++ src/main/java/de/throughput/ircbot/handler/NeinDochMessageHandler.java
@@ -7,6 +7,8 @@ import de.throughput.ircbot.api.MessageHandler;
 
 /**
  * Nein-Doch message handler.
+ *
+ * If someone utters "nein!", it will respond with "doch!" after a short delay.
  */
 @Component
 public class NeinDochMessageHandler implements MessageHandler {
@@ -18,29 +20,17 @@ public class NeinDochMessageHandler implements MessageHandler {
         try {
             String message = event.getMessage()
                     .trim();
-            switch (message) {
-                case "nein!": {
-                    Thread.sleep(MESSAGE_DELAY);
-                    event.getChannel()
-                            .send()
-                            .message("doch!");
-                    return true;
-                }
-                case "NEIN!": {
-                    Thread.sleep(MESSAGE_DELAY);
-                    event.getChannel()
-                            .send()
-                            .message("DOCH!");
-                    return true;
-                }
-                case "Nein!": {
-                    Thread.sleep(MESSAGE_DELAY);
-                    event.getChannel()
-                            .send()
-                            .message("Doch!");
-                    return true;
-                }
-                default: return false;
+            String answer = switch (message) {
+                case "nein!" -> "doch!";
+                case "NEIN!" -> "DOCH!";
+                case "Nein!" -> "Doch!";
+                default -> null;
+            };
+            if (answer != null) {
+                Thread.sleep(MESSAGE_DELAY);
+                event.getChannel()
+                        .send()
+                        .message(answer);
             }
         } catch (InterruptedException e) {
             Thread.currentThread()
